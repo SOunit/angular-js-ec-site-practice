153002f09fc1a6d54cc0a26c57d1c6db25dacb38
SOUnit <workbookwork7@gmail.com>
2022-12-13 21:18:20 -0800

    add user-card detail and display status

--- src/components/organisms/userCard.component.js
+++ src/components/organisms/userCard.component.js
@@ -3,5 +3,18 @@
 
   angular.module("app").component("userCard", {
     templateUrl: "/src/components/organisms/userCard.html",
+    controller: userCardController,
   });
+
+  userCardController.$inject = ["AuthService"];
+  function userCardController(AuthService) {
+    var $ctrl = this;
+
+    var user = AuthService.user;
+    console.log(user);
+
+    if (user) {
+      $ctrl.user = user;
+    }
+  }
 })();
