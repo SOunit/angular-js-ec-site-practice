d44106774f1f14dd632eae7fee28ff4d3854d80d
SOUnit <workbookwork7@gmail.com>
2022-12-16 00:44:53 -0800

    add ui router

--- main.module.js
+++ main.module.js
@@ -1,5 +1,19 @@
 (function () {
   "use strict";
 
-  angular.module("app", []);
+  angular.module("app", ["ui.router"]).config(RoutesConfig);
+
+  // $stateProvider, $urlRouterProvider are from ui-router package
+  RoutesConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/tab1");
+
+    // setup ui states
+    $stateProvider
+      .state("tab1", {
+        url: "/tab1",
+        templateUrl: "/src/components/pages/randomPosts.html",
+      })
+      .state("tab2", { url: "/tab2", template: "<div>tab2</div>" });
+  }
 })();
