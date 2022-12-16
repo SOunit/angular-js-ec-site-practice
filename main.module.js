1ce98f64171646f612cd1ddd4bc1c61134cd8b76
SOUnit <workbookwork7@gmail.com>
2022-12-16 00:51:51 -0800

    add top page, another page

--- main.module.js
+++ main.module.js
@@ -1,19 +1,5 @@
 (function () {
   "use strict";
 
-  angular.module("app", ["ui.router"]).config(RoutesConfig);
-
-  // $stateProvider, $urlRouterProvider are from ui-router package
-  RoutesConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
-  function RoutesConfig($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/tab1");
-
-    // setup ui states
-    $stateProvider
-      .state("tab1", {
-        url: "/tab1",
-        templateUrl: "/src/components/pages/randomPosts.html",
-      })
-      .state("tab2", { url: "/tab2", template: "<div>tab2</div>" });
-  }
+  angular.module("app", ["ui.router"]);
 })();
